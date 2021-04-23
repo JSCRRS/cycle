cf70aabd2697f17187d4058eef65ed8022f605b2
Joshua Musiol <joshua.musiol@web.de>
2021-04-23 15:48:16 +0200

    toggles navigation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,16 +5,20 @@ import Options from "./Options";
 import Timer from "./Timer";
 
 export default function App() {
-    const [optionBar, setOptionBar] = useState(false);
+    /*  const [optionBar, setOptionBar] = useState(false);
 
     function onHamburgerClick() {
         optionBar ? setOptionBar(false) : setOptionBar(true);
-    }
+    } */
 
-    function renderOptionBar() {
+    /*   function renderNavigation() {
         if (optionBar) {
             return <Options />;
         }
+    } */
+
+    function clickHandler() {
+        document.body.classList.toggle("nav-open");
     }
 
     function onFinish() {
@@ -25,11 +29,22 @@ export default function App() {
         <>
             <header>
                 <h1>Cycle</h1>
-                <Hamburger onClick={onHamburgerClick} />
+                <Hamburger onClick={/* onHamburgerClick */ clickHandler} />
             </header>
-            {renderOptionBar()}
-
+            {/*             {renderNavigation()}
+             */}
             <div className="page-center">
+                <nav className="hamburger-options">
+                    <div className="cycle-count">
+                        <p>How many Cycles?</p>
+                    </div>
+                    <div className="cycle-time">
+                        <p>How long per Cycle?</p>
+                    </div>
+                    <div className="pause-time">
+                        <p>How long per pause?</p>
+                    </div>
+                </nav>
                 <div className="timer">
                     <Timer totalTime={5 * 1000} onFinish={onFinish} />
                 </div>
